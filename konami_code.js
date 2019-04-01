9c3d966f5227b7d3600dfeb8483a5f544e0a78ce
Mika88 <mikaspivak@gmail.com>
2019-04-01 22:39:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+
+function init() {
+  
 let index = 0;
 
-function init(e) {
+document.body.addEventListener('keydown', function(e){
   let key = parseInt(e.which);
 
   if (key === code[index]) {
@@ -15,7 +19,6 @@ function init(e) {
     }
   } else {
     index = 0;
-  }
-}
-
-document.body.addEventListener('keydown', init());
+   }
+ }
+};
